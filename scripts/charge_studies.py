af13ed10d1c287deed4f89dcd373968021eb6073
Diego Costas Rodriguez <dcostasr@lxplus945.cern.ch>
2025-02-26 10:56:58 +0100

    now we can produce charge plots for every PMT inside one mPMT

--- scripts/charge_studies.py
+++ scripts/charge_studies.py
@@ -5,8 +5,10 @@ import sys
 import pandas            as pd
 import numpy             as np
 import matplotlib.pyplot as plt
+import awkward           as ak
 
-from tqdm import tqdm
+from tqdm                            import tqdm
+from matplotlib.backends.backend_pdf import PdfPages
 
 # %%
 def open_pickle_file(file):
@@ -18,9 +20,7 @@ def open_pickle_file(file):
 
 # %%
 run        = 514
-run_time   = 32
-pmt_num    = 19
-mpmt_cards = 133
+pdf_file = "figures/"+str(run)+"_card100.pdf"
 
 # %%
 charges  = open_pickle_file("data/"+str(run)+"_final_hit_pmt_charges.pickle")
@@ -30,23 +30,54 @@ channels = open_pickle_file("data/"+str(run)+"_final_hit_pmt_channel.pickle")
 # %%
 # The card_id array is related to the charges array as it gives the card number in 
 # which the hit is produced.
-cards  = card_id[0]
-charge = charges[0]
+# card    = card_id[0]
+# charge  = charges[0]
+# channel = channels[0]
 
-charges_from_card_100 = []
-for i in tqdm(range(len(cards)), total=len(cards), desc="Events", colour="blue"):
-    for j in cards[i]:
-        if j == 100:
-            charges_from_card_100.append(charge[i][j])
+# charge_from_card_100_channel_1 = [
+#     chg[(c == 100) & (ch == 1)] for c, ch, chg in tqdm(zip(card, channel, charge), 
+#                                                        total=len(card))
+# ]
+
+# charge_from_card_100_channel_1 = ak.flatten(charge_from_card_100_channel_1)
+# plt.hist(charge_from_card_100_channel_1, bins=100, histtype="step")
+# plt.yscale('log')
+# plt.xlabel("Charge")
+
+# card = np.array(card, dtype=object)
+# channel = np.array(channel, dtype=object)
+# charge = np.array(charge, dtype=object)
 
 # %%
-c = [[charge[i][j] for j in cards[i] if j == 100] for i in range(len(cards))]
-# %%
-plt.hist(charges_from_card_100, bins=100, histtype="step")
-plt.yscale('log')
-plt.xlabel("Charge")
+pdf = PdfPages(pdf_file)
 
-# %%
+for pmt in range(19):
+    total_charge = []
+    for i in tqdm(range(len(card_id)), total=len(card_id), colour="blue"):
+        card    = card_id[i]
+        charge  = charges[i]
+        channel = channels[i]
 
+        charge_from_card_100_channel_1 = [
+            chg[(c == 100) & (ch == pmt)] for c, ch, chg in tqdm(zip(card, channel, charge), 
+                                                            total=len(card), 
+                                                            colour="yellow", 
+                                                            leave=False)
+        ]
 
+        charge_from_card_100_channel_1 = ak.flatten(charge_from_card_100_channel_1)
+
+        total_charge.append(charge_from_card_100_channel_1)
+
+    total_charge = ak.flatten(total_charge)
+
+    fig, ax = plt.subplots(figsize=(20,12))
+
+    ax.hist(total_charge, bins=10000, histtype="step")
+    ax.set_yscale('log')
+    ax.set_xlabel(f"Charge - Card 100 - Channel {pmt}")
+    # ax.set_xlim(-150, 10000)
+    pdf.savefig(fig)
+
+pdf.close()
 # %%
